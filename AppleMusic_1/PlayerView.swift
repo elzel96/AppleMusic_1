b18975e3f77779cb0c8f28ee4e163a0bc7d0f1be
elzel <elzel666@gmail.com>
2024-02-02 04:14:17 +0300

    fixed_player

--- AppleMusic_1/PlayerView.swift
+++ AppleMusic_1/PlayerView.swift
@@ -4,9 +4,16 @@ struct PlayerView: View {
     var body: some View {
         HStack(spacing: 10) {
             Rectangle()
-                .fill(Color(UIColor.systemGray))
+                .fill(Color(UIColor.systemGray4))
                 .frame(width: 60, height: 60)
                 .cornerRadius(4)
+                .shadow(radius: 2)
+                .overlay(
+                    Image(systemName: "music.note")
+                                       .resizable()
+                                       .foregroundColor(.white)
+                                       .frame(width: 20, height: 30)
+                    )
                 .padding(.leading, 30)
             Text("Не исполняется")
                 .font(.system(size: 18))
@@ -17,7 +24,7 @@ struct PlayerView: View {
                 .padding(.leading, 30)
             Image(systemName: "forward.fill")
                 .font(.system(size: 40))
-                .foregroundColor(Color(UIColor.systemGray))
+                .foregroundColor(Color(UIColor.systemGray4))
         }
         .frame(width: UIScreen.main.bounds.width, alignment: .leading)
         .frame(height: 80)
